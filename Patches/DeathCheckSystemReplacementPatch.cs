af1d29cf7dd77bcb49d1436b491c5bb05f5664e1
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2023-11-27 18:01:15 -0600

    Add project files.

---
+++ Patches/DeathCheckSystemReplacementPatch.cs
@@ -0,0 +1,25 @@
+﻿using Game.Simulation;
+
+namespace Wayz.CS2.MagicalHearse;
+
+[HarmonyPatch]
+public static class MagicalHearseSystemInjector
+{
+    [HarmonyPatch(typeof(DeathCheckSystem), "OnCreate")]
+    [HarmonyPrefix]
+    public static bool OnCreate_Prefix(DeathCheckSystem __instance)
+    {
+        __instance.World.GetOrCreateSystemManaged<MagicalHearseSystem>();
+        __instance.World.GetOrCreateSystemManaged<Game.UpdateSystem>().UpdateAt<MagicalHearseSystem>(Game.SystemUpdatePhase.GameSimulation);
+        return true;
+    }
+
+    [HarmonyPatch(typeof(DeathCheckSystem), "OnUpdate")]
+    [HarmonyPrefix]
+    public static bool OnUpdate_Prefix(DeathCheckSystem __instance)
+    {
+        __instance.World.GetOrCreateSystemManaged<MagicalHearseSystem>().Update();
+        return true;
+    }
+}
+
---
+++ Systems/MagicalHearseSystem.cs
@@ -0,0 +1,45 @@
+﻿using Game;
+using Game.Citizens;
+using Game.Common;
+using Game.Tools;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Wayz.CS2.MagicalHearse;
+public class MagicalHearseSystem : GameSystemBase
+{
+    private EntityCommandBuffer _commandBuffer;
+
+    private EntityQuery _deadCitizenQuery;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        _commandBuffer = base.World.GetOrCreateSystemManaged<EndFrameBarrier>().CreateCommandBuffer();
+
+        _deadCitizenQuery = GetEntityQuery(new EntityQueryDesc()
+        {
+            All = new []
+            {
+                ComponentType.ReadOnly<Citizen>(),
+                ComponentType.ReadOnly<HealthProblem>()
+            },
+            None = new []
+            {
+                ComponentType.ReadOnly<Deleted>(),
+                ComponentType.ReadOnly<Temp>()
+            }
+        });
+        MagicalHearseMod.GameLogger.LogInfo("Injected MagicalHearseSystem!");
+    }
+
+    protected override void OnUpdate()
+    {
+        var deadCitizens = _deadCitizenQuery.ToEntityArray(Allocator.Temp);
+
+        foreach (var entity in deadCitizens)
+        {
+            _commandBuffer.AddComponent<Deleted>(entity);
+        }
+    }
+}
