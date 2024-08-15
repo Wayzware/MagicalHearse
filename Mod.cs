5268d242d00babbb8fe243bc81195528a0f0d9a6
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2024-08-15 18:38:56 -0500

    add settings to burst implementation

--- Mod.cs
+++ Mod.cs
@@ -1,7 +1,9 @@
-﻿using Colossal.Logging;
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
 using Game;
 using Game.Modding;
 using Game.SceneFlow;
+using Unity.Entities;
 
 namespace MagicalHearse
 {
@@ -16,6 +18,8 @@ namespace MagicalHearse
 #endif
         );
 
+        public static Setting m_Setting;
+
         public void OnLoad(UpdateSystem updateSystem)
         {
             Log.Info(nameof(OnLoad));
@@ -23,7 +27,15 @@ namespace MagicalHearse
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
                 Log.Info($"Current mod asset at {asset.path}");
 
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+            AssetDatabase.global.LoadSettings(nameof(MagicalHearse), m_Setting, new Setting(this));
+            Setting.Instance = m_Setting;
+
             updateSystem.UpdateAt<MagicalHearseSystem>(SystemUpdatePhase.GameSimulation);
+
+            World.DefaultGameObjectInjectionWorld.GetOrCreateSystemManaged<MagicalHearseSystem>().Enabled = m_Setting.EnableMagicalHearse;
         }
 
         public void OnDispose()
